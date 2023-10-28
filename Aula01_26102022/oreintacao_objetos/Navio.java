5d26865b576ac0e5f0c7c0e4988f5db14f35212f
leona <leonardovieiraxy@hotmail.com>
2023-10-28 16:30:29 -0300

    Changes to be committed: 	new file:   Aula01_26102022/oreintacao_objetos/Carro.java 	new file:   Aula01_26102022/oreintacao_objetos/MeioDeTransporte.java 	new file:   Aula01_26102022/oreintacao_objetos/Navio.java 	new file:   Aula01_26102022/oreintacao_objetos/Principal.java

---
+++ Aula01_26102022/oreintacao_objetos/Navio.java
@@ -0,0 +1,13 @@
+package oreintacao_objetos;
+
+public class Navio extends MeioDeTransporte {
+    private int qtdeCabines;
+
+    public int getQtdeCabines() {
+        return qtdeCabines;
+    }
+
+    public void setQtdeCabines(int qtdeCabines) {
+        this.qtdeCabines = qtdeCabines;
+    }
+}
